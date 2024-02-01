ac3adc01be2d2f409f4ddd1ac5047fd751d96e9e
Horkustine <tyler.carneschristie@stu.barren.kyschools.us>
2024-02-01 10:22:25 -0600

    created the literal math method exsercise js script

---
+++ LiteralMathMethodExercise.js
@@ -0,0 +1,16 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+warmHugs = warmHugs.replace('like', 'love');
+console.log(warmHugs);
+
+
+let beenImpaled = "Oh, look at that. I've been impaled.";
+console.log(beenImpaled.substring(18));
+
+let dotDotDot = "...";
+let skullBones = "I don't have a skull" + dotDotDot + "or bones."
+console.log(skullBones);
+console.log(Math.PI);
+let randomNumber = Math.random() * 3;
+randomNumber = Math.floor(randomNumber) +1;
+console.log(randomNumber);
